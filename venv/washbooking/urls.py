046af1fef398d88f173ce26b5a0baeac81f668ac
Ludvig Bennbom <ludvig.bennbom.8627@student.uu.se>
2023-03-29 15:08:16 +0200

    LOGIN

--- venv/washbooking/urls.py
+++ venv/washbooking/urls.py
@@ -1,9 +1,21 @@
 from django.urls import path
 from . import views
 from django.conf import settings
+from rest_framework.authtoken.views import obtain_auth_token
+from .views import CreateUserAPIView, LogoutUserAPIView
+from django.urls import re_path
 
 urlpatterns = [
     path('get/', views.getBooking),
     path('post/', views.postBooking),
-    path('delete/<int:pk>/', views.deleteBooking)
+    path('delete/<int:pk>/', views.deleteBooking),
+    path('auth/login/',
+        obtain_auth_token,
+        name='auth_user_login'),
+    path('auth/register/',
+        CreateUserAPIView.as_view(),
+        name='auth_user_create'),
+    path('auth/logout/',
+        LogoutUserAPIView.as_view(),
+        name='auth_user_logout')
 ]
