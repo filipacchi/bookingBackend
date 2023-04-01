1ae5452231ab4a4e6bdebb95da77f21c73682ed9
MARR00M <m.romedahl@gmail.com>
2023-04-01 13:58:25 +0200

    snyggt

--- reactnativeapp/src/screens/NavButtons.js
+++ reactnativeapp/src/screens/NavButtons.js
@@ -1,5 +1,7 @@
 import React from "react";
-import { SafeAreaView, View } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+import { TextInput }from "react-native-paper";
+import { StyleSheet, SafeAreaView, View, Text, Pressable, PermissionsAndroid } from "react-native"
 import LottieView from "lottie-react-native";
 import { Button } from "react-native-paper";
 import { useNavigation } from "@react-navigation/native";
@@ -9,22 +11,54 @@ const NavButtonScreen = () => {
     const navigation = useNavigation()
 
     return (
+        <LinearGradient colors={["#53d5d5", "#2f9d9d"]} style={{ flex: 1 }}>
+            <View style={{
+                position: "absolute",
+                bottom: 150,
+                width: "100%",
+                gap: 20
+            }}>
         <SafeAreaView>
-            <Button title='Till Booking' onPress={() => {navigation.navigate('Booking')}} />
-            <Button title='Till Nav' onPress={() => {navigation.navigate('Nav')}} />
-            <Button title='Till Login' onPress={() => {navigation.navigate('Login')}} />
-            <Button title='Till Splash' onPress={() => {navigation.navigate('Splash')}} />
-
-
-
-
-
+            <Pressable style={styles.input} onPress={() => {navigation.navigate('Booking')}}><Text style={styles.inputText}>Booking</Text></Pressable>
+            <Pressable style={styles.input} onPress={() => {navigation.navigate('Nav')}}><Text style={styles.inputText}>Nav</Text></Pressable>
+            <Pressable style={styles.input} onPress={() => {navigation.navigate('Login')}}><Text style={styles.inputText}>Log in</Text></Pressable>
+            <Pressable style={styles.input} onPress={() => {navigation.navigate('Splash')}}><Text style={styles.inputText}>Splash</Text></Pressable>
         </SafeAreaView>
+        </View>
+        </LinearGradient>
     )
-
-
 }
 
+const styles = StyleSheet.create({
+    input: {
+        width: "70%",
+        borderRadius: 10,
+        borderColor: "white",
+        borderStyle: "solid",
+        justifyContent: "center",
+        borderWidth: 2,
+        alignItems: "center",
+        alignSelf: "center",
+        padding: 15,
+        flexDirection: "row",
+        gap: 10
+    },
+    inputText: {
+        color: "white",
+        fontWeight: 600
+    },
+    inputCredentials: {
+        width: "70%",
+        height: 30,
+        borderRadius: 10,
+        justifyContent: "center",
+        alignItems: "center",
+        alignSelf: "center",
+        padding: 15,
+        flexDirection: "row",
+        gap: 10
+    }
 
+})
 
 export default NavButtonScreen;
