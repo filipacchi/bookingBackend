136e6e208bff977ac880f9e870d089d3e0261199
MARR00M <m.romedahl@gmail.com>
2023-04-03 12:27:59 +0200

    models

--- venv/washbooking/models.py
+++ venv/washbooking/models.py
@@ -6,3 +6,24 @@ class Booking(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=200)
     date = models.DateField()
+
+class User(models.Model):
+    userName = models.CharField(max_length=200, primary_key=True, unique=True, blank=False)
+    language = models.CharField(max_length=200)
+    email = models.CharField(max_length=200, unique=True,blank=False)
+    password = models.CharField(max_length=200, blank=False)
+
+class Association(models.Model):
+    adress = models.CharField(max_length=200, blank=False)
+    coordinateX = models.FloatField(max_length=200, blank=False)
+    coordinateY = models.FloatField(max_length=200, blank=False)
+
+class BookableObjects(models.Model):
+    ...
+
+class BookedTimes(models.Model):
+    startTime = models.DateTimeField(unique=True, blank=False)
+    endTime = models.DateTimeField(unique=True, blank=False)
+
+class Keys(models.Model):
+    used = models.BooleanField(blank=False)
