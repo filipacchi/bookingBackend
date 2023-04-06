ca1db8c088ad46c7729bb6ebdae52bd13b6d7763
filipacchi <ludvig.bennbom.8627@student.uu.se>
2023-04-06 11:00:01 +0200

    auth

--- venv/washbooking/serializer.py
+++ venv/washbooking/serializer.py
@@ -1,23 +1,21 @@
 from rest_framework import serializers
-from .models import Booking
-from .models import Association
+from .models import *
 from django.contrib.auth import get_user_model;
 
-class CreateUserSerializer(serializers.ModelSerializer):
-    username: serializers.CharField()
-    password: serializers.CharField(write_only=True, style={'input_type': 'password'})
+
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
-        model = get_user_model()
-        fields = ('username', 'password', 'first_name', 'last_name')
-        write_only_fields = ('password')
-        read_only_fields = ('is_staff', 'is_superuser', 'is_active')
-    
+        model = UserData
+        fields = ["id", "email", "name", "password"]
+
     def create(self, validated_data):
-        user = super(CreateUserSerializer, self).create(validated_data)
+        user = UserData.objects.create(email=validated_data['email'],
+                                       name=validated_data['name']
+                                         )
         user.set_password(validated_data['password'])
         user.save()
         return user
-
+    
 class BookingSerializer(serializers.ModelSerializer):
     class Meta:
         model=Booking
