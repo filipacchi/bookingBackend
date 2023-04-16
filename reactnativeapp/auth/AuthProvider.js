6c8f0738a8cce411dbd67ed73f7ad7fc4373debf
Ludvig Bennbom <ludvig.bennbom.8627@student.uu.se>
2023-04-16 22:59:31 +0200

    dunno

--- reactnativeapp/auth/AuthProvider.js
+++ reactnativeapp/auth/AuthProvider.js
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import * as SecureStore from 'expo-secure-store';
-import axios from '../axios/axios';
+import axios from '../axios/useAxios';
 import Stack from '../navigation/AppStack';
 
 
