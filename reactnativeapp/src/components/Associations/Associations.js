784003bc8cfa5e862160a9ecf43c417f43115257
filipacchi <ludvig.bennbom.8627@student.uu.se>
2023-04-17 18:48:01 +0200

    ch

--- reactnativeapp/src/components/Associations/Associations.js
+++ reactnativeapp/src/components/Associations/Associations.js
@@ -4,15 +4,16 @@ import { useState } from 'react';
 import React from 'react';
 import axios from "../../../axios/axios";
 import * as SecureStore from 'expo-secure-store';
-export default function Associations() {
 
+
+export default function Associations() {
 
     const [token, setToken] = useState("")
     const [Associations, setAssociation] = useState([])
 
-    const loadData = () => {
-        async function getUserAssociation() {
-            console.log("Inuti getUser: "+token)
+    const loadData = (token) => {
+        async function getUserAssociation(token) {
+            console.log("Inuti getUser: " + token)
             const config = {
                 headers: { Authorization: `Bearer ${token}` }
             };
@@ -31,51 +32,44 @@ export default function Associations() {
                     console.log(error);
                 });
         }
-        getUserAssociation()
+        getUserAssociation(token)
     }
 
     React.useEffect(() => {
         const getToken = async () => {
-            try {
-                let access_token = await SecureStore.getItemAsync('userToken')
-                console.log("ASSO: "+access_token)
-                setToken(access_token)
-                loadData()
-            } catch (e) {
-                console.log("Error when retrieving SecureStore.")
-            }
+            let access_token = await SecureStore.getItemAsync('userToken')
+            console.log("ASSO: " + access_token)
+            setToken(access_token)
+            loadData(access_token)
+            
+            
+
         }
         getToken()
-    },[])
-
-    const getTokenn = async () =>{
-        let token = await SecureStore.getItemAsync('userToken')
-        console.log(token)
-    }
-
+    }, [])
 
 
     return (
         <View style={{ flex: 1 }}>
             <Text style={styles.text}>Associations</Text>
             <Pressable onPress={() => {
-                getTokenn()
+                console.log("TOKEN ÄR: " + token)
             }}><Text style={styles.text}>Klicka maaj</Text></Pressable>
             <FlatList
-                        data={Associations}
-                        style={{}}
-                        renderItem={
-                            ({ item }) =>
-                                <View style={{ borderRadius: 10, overflow: 'hidden', margin: 10 }}>
-                                    <Text suppressHighlighting={true}
-                                        onPress={() => {
-                                            console.log(item.name)
-                                        }}
-                                        style={{ fontSize: 28, padding: 20}}>{item.name}</Text></View>}
+                data={Associations}
+                style={{}}
+                renderItem={
+                    ({ item }) =>
+                        <View style={{ borderRadius: 10, overflow: 'hidden', margin: 10 }}>
+                            <Text suppressHighlighting={true}
+                                onPress={() => {
+                                    console.log(item.name)
+                                }}
+                                style={{ fontSize: 28, padding: 20 }}>{item.name}</Text></View>}
 
-                    >
+            >
 
-                    </FlatList>
+            </FlatList>
         </View>
     )
 }
--- reactnativeapp/src/screens/MainNav.js
+++ reactnativeapp/src/screens/MainNav.js
@@ -11,36 +11,54 @@ import { TabRouter } from "@react-navigation/native";
 import { AntDesign } from '@expo/vector-icons';
 import { Ionicons } from '@expo/vector-icons';
 import NavButtons from "../screens/NavButtons"
+import { LinearGradient } from "expo-linear-gradient";
+import { Header } from "@react-navigation/native"; ;
 
 
 
 const Tab = createBottomTabNavigator();
 
-export default function MainNav({route}) {
+export default function MainNav({ route }) {
 
     console.log(route.params.stateValue)
     const state = route.params.stateValue
-    
+
+
     return (
         <Tab.Navigator
             screenOptions={{
                 tabBarActiveTintColor: "#2f9d9d",
-            
+                tabBarStyle: {
+                    height: 90,
+                    padding: 10
+                },
+                headerStyle: {
+                    backgroundColor: "#53d5d5"
+                }
+
             }}
         >
-            <Tab.Screen name="Associations" component={NavButtons} options={{tabBarIcon:({focused})=>(  
-              <AntDesign name="home" size={25} color={this.tabBarActiveTintColor}/>
-          )  }} />
-                <Tab.Screen name="Info" component={Info} options={{tabBarIcon:({tintColor})=>(  
-                  <Ionicons name="chatbubble-outline" size={25} color={tintColor} />
-              )  }}/>
-            <Tab.Screen name="Schedule" component={Schedule} options={{tabBarIcon:({tintColor})=>(  
-              <AntDesign name="calendar" size={25} color={tintColor}/>
-          )  }}/>
-          
-            <Tab.Screen name="Settings" component={Settings} options={{tabBarIcon:({tintColor})=>(  
-              <AntDesign name="user" size={25} color={tintColor}/>
-          )  }}/>
+            <Tab.Screen name="Associations" component={Associations} options={{
+                tabBarIcon: ({ focused, color }) => (
+                    <AntDesign focused={focused} name="home" size={25} color={color} />
+                )
+            }} />
+            <Tab.Screen name="Info" component={NavButtons} options={{
+                tabBarIcon: ({ focused, color }) => (
+                    <Ionicons focused={focused} name="chatbubble-outline" size={25} color={color} />
+                )
+            }} />
+            <Tab.Screen name="Schedule" component={Schedule} options={{
+                tabBarIcon: ({ focused, color }) => (
+                    <AntDesign focused={focused} name="calendar" size={25} color={color} />
+                )
+            }} />
+
+            <Tab.Screen name="Settings" component={Settings} options={{
+                tabBarIcon: ({ focused, color }) => (
+                    <AntDesign focused={focused} name="user" size={25} color={color} />
+                )
+            }} />
         </Tab.Navigator>
     )
 }
