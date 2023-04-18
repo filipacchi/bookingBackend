c6c1df4b3ec56cd33e6f0efe218c0294e35b7368
filipacchi <ludvig.bennbom.8627@student.uu.se>
2023-04-18 21:17:17 +0200

    test

--- reactnativeapp/axios/axios.js
+++ reactnativeapp/axios/axios.js
@@ -3,4 +3,5 @@ import axios from 'axios';
 export default axios.create({
     baseURL: 'http://130.243.211.140:8000/'
     /*axios.defaults.headers.common = {'Authorization': `Bearer ${token}`}*/
+    //TJENA TJENA TJENA JAG TESTAR ATT LÄGGA TILL LITE, VARFLR FUNKAR INTE GITHUB
 });
