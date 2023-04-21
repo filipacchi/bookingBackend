7784689d74daf3f7aa7bb16b319e3e46f9d7bbda
kalleolssonuu <karlleolsson@gmail.com>
2023-04-21 10:13:04 +0200

    innan ludde views pull

--- reactnativeapp/src/components/Schedule/Schedule.js
+++ reactnativeapp/src/components/Schedule/Schedule.js
@@ -1,14 +1,23 @@
 
-import { StyleSheet, View, Text, Pressable } from "react-native"
+import { StyleSheet, View, Text, Pressable, FlatList } from "react-native"
 import React from 'react';
 
 export default function Schedule() {
 
+    myBookings = {}
 
 
     return (
         <View style={{ flex: 1 }}>
             <Text style={styles.text}>Schedule</Text>
+
+            <FlatList
+            data={myBookings}
+            onRender={( (item) => {
+                
+            })}>
+
+            </FlatList>
         </View>
     )
 }
