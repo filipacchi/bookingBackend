89f9eca02763aff7e931843efb7f4ee731da7a21
Ludvig Bennbom <ludvig.bennbom.8627@student.uu.se>
2023-04-21 22:55:45 +0200

    lang

--- reactnativeapp/src/screens/Auth.js
+++ reactnativeapp/src/screens/Auth.js
@@ -3,7 +3,7 @@ import { Card } from "react-native-paper"
 import React from 'react';
 import { AntDesign } from '@expo/vector-icons';
 import { LinearGradient } from "expo-linear-gradient";
-import { useState, setState } from "react";
+import { useState, setState, useContext } from "react";
 import { TextInput } from "react-native-paper";
 import { AuthContext } from "../../navigation/AppStack";
 import Logo from "../components/assets/Logo";
@@ -13,6 +13,7 @@ import { useNavigation } from "@react-navigation/native";
 
 export default function Auth() {
     const navigation = useNavigation()
+    const {t} = useContext(AuthContext)
     return (
         <LinearGradient colors={["#53d5d5", "#2f9d9d"]} style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
             <Logo></Logo>
@@ -22,8 +23,8 @@ export default function Auth() {
                 width: "100%",
                 gap: 20
             }}> 
-                <Pressable style={[styles.input, styles.presslogin]} onPress={() => {navigation.navigate('Login') }}><Text style={[styles.inputText, styles.login]}>Logga in</Text></Pressable>
-                <Pressable style={[styles.input, styles.presslogin]} onPress={() => {navigation.navigate('Register') }}><AntDesign name="adduser" size={20} color="white" /><Text style={styles.inputText}>Skapa konto</Text></Pressable>
+                <Pressable style={[styles.input, styles.presslogin]} onPress={() => {navigation.navigate('Login') }}><Text style={[styles.inputText, styles.login]}>{t("Login")}</Text></Pressable>
+                <Pressable style={[styles.input, styles.presslogin]} onPress={() => {navigation.navigate('Register') }}><AntDesign name="adduser" size={20} color="white" /><Text style={styles.inputText}>{t("CreateAccount")}</Text></Pressable>
             </View>
         </LinearGradient>
     )
