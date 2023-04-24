9ecdce709426a1da030ef0ffcc79e393f522cf81
Kalle Olsson <karlleolsson@gmail.com>
2023-04-24 10:46:19 +0200

    småskit innan fetch och reset

--- reactnativeapp/axios/axios.js
+++ reactnativeapp/axios/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'http://192.168.0.16:8000'
+    baseURL: 'http://192.168.1.214:8000' /* samma som backend, som i sin tur startar på samma ip som frontend */
     /*axios.defaults.headers.common = {'Authorization': `Bearer ${token}`}*/
     //TJENA TJENA TJENA JAG TESTAR ATT LÄGGA TILL LITE, VARFLR FUNKAR INTE GITHUB
 });
