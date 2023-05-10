98f22295888fa39b0124b5de2c01bc4ebeb00e82
filipacchi <ludvig.bennbom.8627@student.uu.se>
2023-05-10 21:11:14 +0200

    added remote db

--- venv/bookingproject/bookingproject/settings.py
+++ venv/bookingproject/bookingproject/settings.py
@@ -15,6 +15,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 #Here we go again MF
 from pathlib import Path
 from datetime import timedelta
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -95,12 +96,23 @@ WSGI_APPLICATION = 'bookingproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
+""" DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-}
+} """
+
+DATABASES = {  
+    'default': {  
+        'ENGINE': 'django.db.backends.mysql',  
+        'NAME': 'book_booking',  
+        'USER': 'book_django',  
+        'PASSWORD': os.environ.get("BPASSWORD"),  
+        'HOST': os.environ.get("BHOST"),  
+        'PORT': '3306',  
+    }  
+}  
 
 
 # Password validation
