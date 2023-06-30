91af3fd167673cf633b041c4a159f012329017f5
Kalle Olsson <karlleolsson@gmail.com>
2023-06-30 18:16:14 +0200

    sync efter registrering och

--- reactnativeapp/axios/axios.js
+++ reactnativeapp/axios/axios.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
 
 export default axios.create({
-   /*  baseURL: 'https://bookease.se' */
-   baseURL: "http://192.168.68.114:8000/"
+   // baseURL: 'https://bookease.se'
+   // baseURL: "http://192.168.68.114:8000/" /*  */
+   baseURL: "http://192.168.1.214:8000/" /* Kalle */
     /*axios.defaults.headers.common = {'Authorization': `Bearer ${token}`}*/
     //TJENA TJENA TJENA JAG TESTAR ATT LÄGGA TILL LITE, VARFLR FUNKAR INTE GITHUB
 });
